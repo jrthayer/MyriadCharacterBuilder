7fe7b382263161e9edb0cfc25fe54890f459c4d7
James Thayer <jrthayer@users.noreply.github.com>
2020-11-15 14:55:11 -0800

    Update myriadBuilder.js

--- myriadBuilder.js
+++ myriadBuilder.js
@@ -1,5 +1,16 @@
 var file = new XMLHttpRequest();
+
+// file.onload = function(){
+//     console.log("load file");
+//     console.log(this);
+// }
+
+// file.onerror() = function(){
+//     console.log("error");
+// }
+
 file.open("GET", "MyriadAbilityInfo.txt");
+file.send();
 console.log(file);
 var text = file.responseText;
 document.write(text);
