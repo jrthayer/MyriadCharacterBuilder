d8f78f0c878759c5d4bdd9e096bde3001263d2da
James Thayer <jrthayer@users.noreply.github.com>
2020-11-15 21:22:53 -0800

    Add symbols for individual lines of abilities and start parsing
    Currently parses all abilities and seperates tag from description but removes name of ability atm.
    
    Still Needs:
    -remove white space arount description
    -add back in ability name
    -add tag "name" to ability name
    -determine indexes for skill tree seperation
    -format abilities into json

--- DataParsing/parse.py
+++ DataParsing/parse.py
@@ -1,4 +1,13 @@
 test = open("MyriadAbilityInfo.txt", "r");
 contents = test.read();
-Abilities = contents.split("*");
-print(Abilities[1]);
+abilities = contents.split("*");
+
+for index, ability in enumerate(abilities):
+    abilityLines = ability.split("#");
+    iterAbilityLines = iter(abilityLines);
+    next(iterAbilityLines);
+    for index2, line in enumerate(iterAbilityLines):
+        abilityLines[index2] = line.split(" ", 1);
+    abilities[index] = abilityLines;
+
+print(abilities[1]);
