2b7b77ab7e88a002bcedbb2ccdfae08dee27cf2e
James Thayer <jrthayer@users.noreply.github.com>
2020-11-15 17:38:39 -0800

    Testing json conversion to js

--- myriadBuilder.js
+++ myriadBuilder.js
@@ -1,16 +1,26 @@
 var file = new XMLHttpRequest();
-
-// file.onload = function(){
-//     console.log("load file");
-//     console.log(this);
-// }
-
-// file.onerror() = function(){
-//     console.log("error");
-// }
-
-file.open("GET", "MyriadAbilityInfo.txt");
+file.open("GET", "test.json");
 file.send();
+
+file.onreadystatechange = function() {
+    if (file.readyState == 4 && file.status == 200) {
+        var myArr = JSON.parse(file.responseText);
+        console.log("hello");
+        console.log(myArr);
+        test(myArr);
+    }
+}
+
+function test(myArr){
+    console.log(myArr.cars.Nissan.forEach(function(val,ind,ob){me(val,ind,ob)}));
+
+}
+
+function me(val,ind,ob){
+    console.log(val.model);
+    console.log(ind);
+    console.log(ob);
+}
+
 console.log(file);
-var text = file.responseText;
-document.write(text);
+
