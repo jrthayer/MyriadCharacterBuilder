ba350abcebb62eb8a8f43be948dd0392f58efacc
James Thayer <jrthayer@users.noreply.github.com>
2020-11-16 20:20:43 -0800

    Update page parsing with all needed features
    Ability text file is now parse and converted into JSON format. Includes all abilities seperated by line and bookmarks for each ability tree/tier.

--- DataParsing/parse.py
+++ DataParsing/parse.py
@@ -1,13 +1,66 @@
-test = open("MyriadAbilityInfo.txt", "r");
-contents = test.read();
+
+import json;
+
+abilityFile = open("MyriadAbilityInfo.txt", "r");
+contents = abilityFile.read();
+
+#split file into abilities
 abilities = contents.split("*");
 
+
+bookmarks = [["DEX"], ["INT"], ["STR"], ["MAD"]];
+delAbilities = [];
+curStat = -1;
+
+
+#split abilities into their individual lines
 for index, ability in enumerate(abilities):
-    abilityLines = ability.split("#");
+    #Find index values that seperate each stat tree and their ability tiers
+    if "===========" in ability:
+        curStat+=1;
+        bookmarks[curStat].append(index);
+        del abilities[index];
+    elif "[Tier" in ability:
+        if "[Tier4" in ability:
+            classMarker = ability.split("(");
+            className = classMarker[1];
+            className = className.split(")");
+            className = className[0];
+            bookmarks[curStat].append([index, className]);
+        else:
+            bookmarks[curStat].append(index);
+        del abilities[index];
+
+    abilityLines = abilities[index].split("#");
+
+
+    #first line of ability is special case
+    abilityLines[0] = ["Name" , abilityLines[0]]
     iterAbilityLines = iter(abilityLines);
     next(iterAbilityLines);
+
+    #split each line into key pairs
     for index2, line in enumerate(iterAbilityLines):
-        abilityLines[index2] = line.split(" ", 1);
+        abilityLines[index2+1] = line.split(None,1);
+
+    #convert to json
     abilities[index] = abilityLines;
 
-print(abilities[1]);
+bookmarks = bookmarks;
+abilityBookmarkList = [bookmarks, abilities];
+
+with open("ability.json", "w") as file:
+    json.dump(abilityBookmarkList, file);
+
+
+
+#remove sepeartion entites from abilities
+# print(delAbilities);
+# print(sorted(delAbilities, reverse=True));
+# for index in sorted(delAbilities, reverse=True):
+#     del abilities[index];
+#print(abilities[18]);
+
+# print(abilityJN);
+
+
--- myriadBuilder.js
+++ myriadBuilder.js
@@ -1,26 +1,27 @@
 var file = new XMLHttpRequest();
-file.open("GET", "test.json");
+file.open("GET", "DataParsing/ability.json");
 file.send();
 
 file.onreadystatechange = function() {
     if (file.readyState == 4 && file.status == 200) {
-        var myArr = JSON.parse(file.responseText);
-        console.log("hello");
-        console.log(myArr);
+        var abilityFile = JSON.parse(file.responseText);
+        var abilityBookmarks = abilityFile[0];
+        var abilities = abilityFile[1];
+        console.log(abilityBookmarks);
+        console.log(abilities);
         test(myArr);
     }
 }
 
 function test(myArr){
-    console.log(myArr.cars.Nissan.forEach(function(val,ind,ob){me(val,ind,ob)}));
-
+    // console.log(myArr.forEach(function(val,ind,ob){me(val,ind,ob)}));
 }
 
 function me(val,ind,ob){
-    console.log(val.model);
-    console.log(ind);
-    console.log(ob);
+    // console.log(val[1]);
+    // console.log(ind);
+    // console.log(ob);
 }
 
-console.log(file);
+// console.log(file);
 
