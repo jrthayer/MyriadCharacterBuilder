cf3aecc0058df9b45e9853c129d4d0f2ec179e8b
James Thayer <jrthayer@users.noreply.github.com>
2020-11-18 18:04:12 -0800

    Add ability description structure and initial css

--- myriadBuilder.js
+++ myriadBuilder.js
@@ -22,17 +22,39 @@ function createSkillTree(statAbilityArray){
 }
 
 function createAbility(index){
-    var ability = document.createElement('ul');
-    var test = document.createElement('li');
-    test.innerHTML = index[0][1][1] + ": " + index[0][0];
-    ability.appendChild(test);
+    var ability = document.createElement('div');
+    ability.classList.add("ability");
+
+    //Create ability description
+    var abilityDesc = document.createElement('div');
+    abilityDesc.classList.add("abilityDesc");
+    ability.appendChild(abilityDesc);
+
+    var abilityHeader = document.createElement('h3');
+    var table = document.createElement('table');
+
+    abilityHeader.innerHTML = index[0][1][1] + ": " + index[0][0];
+    abilityDesc.appendChild(abilityHeader);
+    abilityDesc.appendChild(table);
+
     for(x = 1; x < index.length; x++){
-        var line = document.createElement('li');
-        line.innerHTML = index[x][0] + ": " + index[x][1];
-        ability.appendChild(line);
+        var row = document.createElement('tr');
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var pre1 = document.createElement('pre');
+        var pre2 = document.createElement('pre');
+        pre1.innerHTML = index[x][0];
+        pre2.innerHTML = index[x][1];
+        cell1.appendChild(pre1);
+        cell2.appendChild(pre2);
+        table.appendChild(row);
     }
     var container = document.getElementById('container');
     container.appendChild(ability);
+}
+
+function createAbililtyDesc(index){
+
 }
 
 
